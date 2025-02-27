7fbfc4a938cd3f1e167fc154a6d1499036864323
Hugo Gabriel <hugogafm3@gmail.com>
2025-02-27 14:17:16 -0300

    Criação do MenuApp, Site de cardápio personalizável para restaurantes.

---
+++ src/test/java/MenuApp/MenuApp/MenuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package MenuApp.MenuApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
